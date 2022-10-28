137f502590213e4eec6fdcbf2734362acd8aadda
Pavel Nasaltsev <nasaltsev@web.de>
2022-10-28 15:58:15 +0200

    4

--- src/main/java/webtech/GreetingController.java
+++ src/main/java/webtech/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
