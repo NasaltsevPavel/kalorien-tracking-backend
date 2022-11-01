24ae927ae61d71f6363189591eaa60c8fede67fa
Pavel Nasaltsev <nasaltsev@web.de>
2022-11-01 20:54:04 +0100

    Update

--- src/main/java/webtech/User/User.java
+++ src/main/java/webtech/User/User.java
@@ -1,13 +1,6 @@
 package webtech.User;
 
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.Collections;
-
 public class User {
 
     private long id;
