e18c6e044b8a74d6dbb45920849941a2050e22fa
Pavel Nasaltsev <nasaltsev@web.de>
2022-11-01 20:52:05 +0100

    Update

--- src/main/java/webtech/User/UserRepository.java
+++ src/main/java/webtech/User/UserRepository.java
@@ -4,7 +4,6 @@ package webtech.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
